6e1854ed2a100c5ef3b114dffed635a420fb7c19
dilshan wijesinghe <38337550+dilshan23@users.noreply.github.com>
2022-10-26 08:15:11 +0530

    Create staircase.py
    DP staircase

---
+++ dynamic_programming/staircase.py
@@ -0,0 +1,14 @@
+def climbStaris(n):
+	if n == 1:
+		return 1
+	else:
+		pass
+
+  # n= 1 -- > 1
+	# n =2   --> 2/1   -- > 2
+	# n = 3 ---> 1,1,1/2,1/1,2 -->3
+
+	### you can either climb 1 or 2 steps.
+
+if __name__ == "__main__":
+	climbStaris(2)
