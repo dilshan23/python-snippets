d8f9a7c09bf443520b48fed22db6d28d2ae38241
dilshan wijesinghe <38337550+dilshan23@users.noreply.github.com>
2022-11-26 20:53:56 +0530

    Update shop.py
    python3 update

--- hackerranks problems/shop.py
+++ hackerranks problems/shop.py
@@ -1,37 +1,14 @@
 def getMoneySpent(keybords,drives,b):
-
-
 	values = []
-	#afetr get the max from values
-
-
 	for i in range(len(keybords)):
 		for j in range(len(drives)):
 			if keybords[i] + drives[j] <= b:
 				sum1 = keybords[i]+drives[j]
 				values.append(sum1)
-			
 
-	#print(values)
-
-
-
-    
-
-	#print max_sum
 	if len(values) > 0:
 		return max(values)
 	else:
 		return "-1"
 				
-
-
-	
-
-    
-
-	
-
-
-
-print getMoneySpent([40,50,60],[40,40,40],60)
+print(getMoneySpent([40,50,60],[40,40,40],60))
