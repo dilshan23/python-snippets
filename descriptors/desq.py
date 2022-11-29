2200f1e52b111b1a70f7a07d3fe8d61f5e22d683
dilshan wijesinghe <38337550+dilshan23@users.noreply.github.com>
2022-11-29 17:14:02 +0530

    Update desq.py

--- descriptors/desq.py
+++ descriptors/desq.py
@@ -1,15 +1,15 @@
-#wh --- type validatins
+#why --- type validatins
 
- class that implements __get_(), __set()_, or __delete()_ function of a descriptor protocol for an object is classified as a “Descriptor”.
+#class that implements __get_(), __set()_, or __delete()_ function of a descriptor protocol for an object is classified as a “Descriptor”.
  
  
  class Descriptor():
    def __init__(self,name=""):
       self.name = name
       
-   def __ser__():  # type checking name must ne string
+   def __set__():  # type checking name must ne string
       if isintane(name,str):
         self.name = name
       else:
-         raise Typeerror("name must be string)
+         raise TypeError("name must be string)
     
