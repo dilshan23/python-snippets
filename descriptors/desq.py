caa9f2255f656330af334763231208f538dc83b4
dilshan wijesinghe <38337550+dilshan23@users.noreply.github.com>
2022-11-30 09:13:42 +0530

    Update desq.py

--- descriptors/desq.py
+++ descriptors/desq.py
@@ -17,4 +17,38 @@
         self.name = name
       else:
          raise TypeError("name must be string)
+                         
+                         ###
+                         
+ class BoundedNumber:
+    def __init__(self, min_val, max_val):
+        self.min_val = min_val
+        self.max_val = max_val
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __set__(self, instance, value):
+        if self.min_val > value or value > self.max_val:
+            msg = '{} takes values between {} and {}'.format(
+                self.name, 
+                self.min_val, 
+                self.max_val,
+            )
+            raise ValueError(msg)
+        instance.__dict__[self.name] = value
+
+    def __get__(self, instance, owner=None):
+        return instance.__dict__[self.name]
+                         
+    class Person:
+       age = BoundedNumber(1, 120)
+       weight = BoundedNumber(1, 250)
+       height = BoundedNumber(1, 230)
+
+       def __init__(self, name, age, weight, height):
+           self.name = name
+           self.age = age
+           self.weight = weight
+           self.height = height
     
