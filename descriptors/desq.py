2cf4b5277537297d9da72280d4829ab4915b57a8
dilshan wijesinghe <38337550+dilshan23@users.noreply.github.com>
2022-11-30 09:11:51 +0530

    Update desq.py

--- descriptors/desq.py
+++ descriptors/desq.py
@@ -1,6 +1,11 @@
 #why --- type validatins
 
 #class that implements __get_(), __set()_, or __delete()_ function of a descriptor protocol for an object is classified as a “Descriptor”.
+
+#How to Use Descriptors
+#Using descriptors makes sense when you have multiple attributes with similar behaviour.
+
+#For example, imagine we are implementing a class called Person with attributes such as name, age, weight and height. We might want to check that the values for some of these attributes make sense (e.g. we can’t have a person with negative age). One way to do this would be to have a bunch of if statements in __init__. However, this can quickly turn ugly if you have to do this for many attributes. Having something like below looks much better and it is easier to read!
  
  
  class Descriptor():
