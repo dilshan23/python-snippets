c4875c22b7c6022bce3e6d32ea93ec787c524072
dilshan wijesinghe <38337550+dilshan23@users.noreply.github.com>
2022-11-30 18:51:20 +0530

    Create difference_with_atproeprty.py

---
+++ descriptors/difference_with_atproeprty.py
@@ -0,0 +1,43 @@
+class Lineitem():
+  def __init__():
+    self.weight = weight
+    self.price = price
+    
+  @property  
+  def weught(self):
+      return self.weight
+    
+  @wifight.setter
+  if wegiht < 0 :
+    raise error
+    
+ 
+## now what proce also need the same logic as weight ??? duplicate setters and getters
+### ----> use descriptors to mange attributes businessl ogic
+
+
+
+Class Qunitty()
+   def __init__():
+    
+    
+   def __set__():
+    
+    
+   def __get__():
+    
+class LineItem():
+  weight = Quantity()
+  height = Quantity()
+  
+  def __init__(weight,height,descripotion):
+    self.weight = weight
+    ...
+
+  
+  
+  
+  
+  
+    
+  
